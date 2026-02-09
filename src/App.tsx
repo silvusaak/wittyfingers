5d0062972bcb10a0e0a8e8bea7f14eeae4f068a3
silvusaak <silvusaak@gmail.com>
2026-02-09 16:02:57 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+       <BrowserRouter basename={import.meta.env.BASE_URL}>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
